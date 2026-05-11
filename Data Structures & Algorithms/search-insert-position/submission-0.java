e77808506bb2742e5c01be52c77e56482cda9eb1
Saurabh Kashyap <kashyap.sau@northeastern.edu>
2026-05-11 18:33:40 -0400

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int low =0;
+        int high=nums.length-1;
+          int mid=0;
+        while(low<=high){
+
+            mid = (low +high)/2;
+
+           if(nums[mid]==target){
+            return mid;
+           }
+
+           else if(nums[mid]<target){
+             low=mid+1;
+           }
+           else{
+            high=mid-1;
+           }
+
+        }
+
+        if(mid==0&&nums[mid]>target){
+            return mid;
+        }
+        else if(mid==0&&nums[mid]<target){
+            return 1;
+        }
+
+        else if(nums[mid]>target){
+            return mid;
+        }
+
+        else{
+            return mid+1;
+        }
+        
+    }
+}
